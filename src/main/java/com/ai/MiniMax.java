92d0389a83d448e12411a77e16f0fa1dc753b747
spasoje.petronijevic <spasoje.petronijevic@computerrock.com>
2021-03-06 22:36:06 +0100

    multithreading added.

--- src/main/java/com/ai/MiniMax.java
+++ src/main/java/com/ai/MiniMax.java
@@ -8,6 +8,7 @@ import java.util.concurrent.*;
 
 public class MiniMax {
 
+    private ExecutorService executorService = Executors.newCachedThreadPool();
     private double alpha = Double.MIN_VALUE;//best found for max
     private double beta = Double.MAX_VALUE;//best found for min
     private BreakTest breakTest;
@@ -25,13 +26,36 @@ public class MiniMax {
 
     private Action search(MiniMaxState state, int depth) {
         Action act = null;
+        List<ImmutablePair<Action, Future<Double>>> evaluations = new ArrayList<>();
+        Set<Action> actions = state.getActions();
         if (state.maxPlayer()) {
-            double max = Double.MIN_VALUE;
-            for (Action action : state.getActions()) {
-                double eval = min(((MiniMaxState) state.newState(action)), depth - 1);
+            for (Action action : actions) {
+                evaluations.add(
+                        new ImmutablePair<>(
+                                action,
+                                executorService
+                                        .submit(() -> min(((MiniMaxState) state.newState(action)), depth - 1))
+                        )
+                );
+            }
+            double max = -1 * Double.MAX_VALUE;
+            for (ImmutablePair<Action, Future<Double>> evaluation : evaluations) {
+                try {
+                    System.out.println(evaluation.getLeft() + ": " + evaluation.getRight().get());
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            for (ImmutablePair<Action, Future<Double>> evaluation : evaluations) {
+                Double eval = null;
+                try {
+                    eval = evaluation.getRight().get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
                 if (eval > max) {
                     max = eval;
-                    act = action;
+                    act = evaluation.getLeft();
                 }
             }
         } else {
