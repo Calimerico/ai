970449c4f72051151a91a7a2e69315c749c08ad0
spasoje <spasojepetronijevic@gmail.com>
2021-08-27 10:12:19 +0200

    BreakTest is interface now

--- src/main/java/com/ai/BreakTest.java
+++ src/main/java/com/ai/BreakTest.java
@@ -1,17 +1,6 @@
 package com.ai;
 
-import lombok.Getter;
-
-public class BreakTest {
-
-    @Getter
-    private final int depth;
-
-    public BreakTest(int depth) {
-        this.depth = depth;
-    }
-
-    boolean shouldEvaluate(MiniMaxState state, int depth) {
-        return depth == 0;
-    }
+public interface BreakTest {
+    boolean shouldEvaluate(MiniMaxState state, int depth);
+    int getDepth();
 }
--- src/test/java/com/MiniMaxTest.java
+++ src/test/java/com/MiniMaxTest.java
@@ -1,22 +1,12 @@
 package com;
 
-import com.ai.*;
 import org.junit.Test;
-
-import java.util.HashSet;
 
 public class MiniMaxTest {
 
 
     @Test
-    public void chessTest() {
-        //todo wtf sta radi ovaj test
-        MiniMax miniMax = new MiniMax(new BreakTest(6));
-        HashSet<Action> actionList = new HashSet<>();
-        actionList.add(new DummyAction(1));
-        actionList.add(new DummyAction(-1));
-        actionList.add(new DummyAction(2));
-        Action action = miniMax.search(new DummyMiniMax(5, true, actionList));
-        System.out.println(action);
+    public void miniMaxTest() {
+        //todo implement
     }
 }
